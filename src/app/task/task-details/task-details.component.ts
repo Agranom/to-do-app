9c477c12d0ee9457372b722f4a2b0b0997fc82a0
Boyko Vitaliy <boyko330@gmail.com>
2017-07-29 12:31:43 +0300

    Added back button for details

--- src/app/task/task-details/task-details.component.ts
+++ src/app/task/task-details/task-details.component.ts
@@ -3,34 +3,39 @@ import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 import {ToDoTasksService} from '../services/to-do-tasks.service';
 import {Task} from '../models/task.interface';
 import 'rxjs/add/operator/switchMap';
+import {Location} from '@angular/common';
 
 
 @Component({
-  selector: 'app-task-details',
-  templateUrl: './task-details.component.html',
-  styleUrls: ['./task-details.component.sass']
+	selector: 'app-task-details',
+	templateUrl: './task-details.component.html',
+	styleUrls: ['./task-details.component.sass']
 })
 export class TaskDetailsComponent implements OnInit {
 
-  task: Task;
-  taskKey: string;
+	task: Task;
+	taskKey: string;
 
-  constructor(private route: ActivatedRoute, private router: Router,
-              private toDoTasksService: ToDoTasksService) {
-  }
+	constructor(private route: ActivatedRoute, private router: Router,
+				private toDoTasksService: ToDoTasksService,
+				private location: Location) {
+	}
 
-  ngOnInit() {
-    this.route.paramMap
-      .switchMap((params: ParamMap) => {
-      this.taskKey = params.get('id');
-        return this.toDoTasksService.getTask(params.get('id'));
-      })
-      .subscribe((task: Task) => this.task = task);
-  }
+	ngOnInit() {
+		this.route.paramMap
+			.switchMap((params: ParamMap) => {
+				this.taskKey = params.get('id');
+				return this.toDoTasksService.getTask(params.get('id'));
+			})
+			.subscribe((task: Task) => this.task = task);
+	}
 
-  editTask(task): void {
-    this.toDoTasksService.updateTask(this.taskKey, task)
-      .then(() => this.router.navigate(['/task']));
-  }
+	editTask(task): void {
+		this.toDoTasksService.updateTask(this.taskKey, task)
+			.then(() => this.router.navigate(['/task']));
+	}
 
+	back(): void {
+		this.location.back();
+	}
 }
