2e89ef6aa1917e59d0a2421bcab8827630f7666e
Agranom777 <vitalii.boiko@n-cube.co.uk>
2017-08-13 11:20:16 +0300

    Created can deactivate guard service

---
+++ src/app/shared/services/can-deactivate-guard.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {CanDeactivate} from "@angular/router";
+
+export interface CanComponentDeactivate {
+	canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+}
+
+@Injectable()
+export class CanDeactivateGuardService implements CanDeactivate<CanComponentDeactivate> {
+	canDeactivate(component: CanComponentDeactivate): boolean | Observable<boolean> | Promise<boolean> {
+		return component.canDeactivate ? component.canDeactivate() : true;
+	}
+}
