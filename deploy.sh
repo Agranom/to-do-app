20c18134368c05e5d481ad201cc15b4d6d7a2835
Agranom777 <vitalii.boiko@n-cube.co.uk>
2017-08-17 16:39:20 +0300

    Travis

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+filesChanged=$(git ls-tree -r branch)
+if [ ${#filesChanged[@]} -eq 0 ]; then
+    echo "No files to update"
+else
+    for f in $filesChanged
+	do
+		#do not upload these files that aren't necessary to the site
+		if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "package.json" ]
+		then
+	 		echo "Uploading $f"
+	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://fagrano.beget.tech/paradise-studio.com.ua/public_html/todo/$f
+		fi
+	done
+fi
