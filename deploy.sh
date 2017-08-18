d6db0da74ce0f7b652061c0ad651cb97fe3c4756
Agranom777 <vitalii.boiko@n-cube.co.uk>
2017-08-18 10:00:13 +0300

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 filesChanged="./dist/*"
-echo filesChanged
+echo $filesChanged
 if [ ${#filesChanged[@]} -eq 0 ]; then
     echo "No files to update"
 else
     for f in $filesChanged
 	do
+
 		#do not upload these files that aren't necessary to the site
 		if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "package.json" ]
 		then
 	 		echo "Uploading $f"
-	 		"curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://fagrano.beget.tech/paradise-studio.com.ua/public_html/todo/$f"
+	 		a="$f"|tail -c +8
+	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASSWORD ftp://fagrano.beget.tech/$a
 		fi
 	done
 fi
